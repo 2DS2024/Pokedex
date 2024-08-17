2cbe3669f7b8ebe03ec7ccb2be4bfb444d898127
GalloJunior <gallojunior@gmail.com>
2024-08-16 22:28:44 -0300

    Estilização da área pública

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Pokedex.Data;
 using Pokedex.Models;
 
@@ -9,14 +8,17 @@ namespace Pokedex.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AppDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, AppDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        
         return View();
     }
 
