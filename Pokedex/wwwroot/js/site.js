947f80476021dce067f9832dd8e5d5e76f4f1119
GalloJunior <gallojunior@gmail.com>
2024-09-20 22:19:50 -0300

    Aplicando filtro JS

--- Pokedex/wwwroot/js/site.js
+++ Pokedex/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function filter(tipo) {
+    // Alterar a visibilidade dos cards
+    let cardCount = 0;
+    document.querySelectorAll('.poke').forEach(card => {
+        card.style.display = "flex";
+        cardCount += 1;
+        if (!card.classList.contains(tipo) && tipo !== ''){
+            card.style.display = 'none';
+            cardCount -= 1;
+        }
+    });
+    
+    // Verificar se existem cards
+    let zeroPokemon = document.querySelector('#zeroPokemon');
+    cardCount == 0 ?
+        zeroPokemon.classList.remove('d-none') :
+        zeroPokemon.classList.add('d-none');
 
-// Write your JavaScript code.
+    // Alterar o estilo dos botões de filtro
+    document.querySelectorAll('.btn-filter').forEach(button =>{
+        button.classList.add('btn-sm');
+        button.classList.remove('btn-md');
+        if (button.id == `btn-${tipo}`) {
+            button.classList.remove('btn-sm');
+            button.classList.add('btn-md');
+        }
+    });
+}
